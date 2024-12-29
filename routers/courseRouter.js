9b42c504ac8fb73fa46e61d4a6caad79d55f9ed9
unknown <mohamed.hnef1@gmail.com>
2024-12-29 02:24:30 +0200

    removed unneccassery routes and controllers.

--- routers/courseRouter.js
+++ routers/courseRouter.js
@@ -7,5 +7,6 @@ router.post('/', authMiddleware.verifyToken, authMiddleware.isFaculty, courseCon
 router.put('/:id', authMiddleware.verifyToken, authMiddleware.isFaculty, courseController.updateCourse);
 router.delete('/:id', authMiddleware.verifyToken, authMiddleware.isFaculty, courseController.deleteCourse);
 router.get('/', authMiddleware.verifyToken, authMiddleware.isFaculty, courseController.getAllCourses);
+router.get('/:id/registration', authMiddleware.verifyToken, authMiddleware.isFaculty, courseController.getCourseRegistrationStatus);
 
 module.exports = { courseRouter: router };
--- routers/facultyRouter.js
+++ routers/facultyRouter.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const { courseController } = require('../controllers/courseController');
+const { facultyController } = require('../controllers/facultyController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-router.post('/', authMiddleware.verifyToken, authMiddleware.isFaculty, courseController.addCourse);
-router.put('/:id', authMiddleware.verifyToken, authMiddleware.isFaculty, courseController.updateCourse);
-router.delete('/:id', authMiddleware.verifyToken, authMiddleware.isFaculty, courseController.deleteCourse);
-router.get('/', authMiddleware.verifyToken, authMiddleware.isFaculty, courseController.getAllCourses);
-router.get('/:id/registration', authMiddleware.verifyToken, authMiddleware.isFaculty, courseController.getCourseRegistrationStatus);
 
-module.exports = { courseRouter: router };
+router.get('/', authMiddleware.verifyToken, authMiddleware.isFaculty, facultyController.getAllFaculty);
+router.get('/:id', authMiddleware.verifyToken, authMiddleware.isFaculty, facultyController.getFaculty);
+
+module.exports = { facultyRouter: router };

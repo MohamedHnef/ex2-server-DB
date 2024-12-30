cd8547fbdf6e546ac9ca6faff509417e73f93659
unknown <mohamed.hnef1@gmail.com>
2024-12-30 03:07:19 +0200

    removed id from get my courses route

--- routers/studentRouter.js
+++ routers/studentRouter.js
@@ -3,6 +3,6 @@ const { studentController } = require('../controllers/studentController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-router.get('/:id/courses', authMiddleware.verifyToken, authMiddleware.isStudent, studentController.getRegisteredCourses);
+router.get('/courses', authMiddleware.verifyToken, authMiddleware.isStudent, studentController.getRegisteredCourses);
 
 module.exports = { studentRouter: router };
